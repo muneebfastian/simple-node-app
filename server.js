abc4fe78dc6d38903dfb474665894528f847312c
Muneeb Ahmad <muneebfastian@gmail.com>
2016-08-21 10:59:53 +0500

    completed task 3

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
 var express = require('express');
 
-var app = express();
+var app  = express();
+var RSVP = require('rsvp');
 
 app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.get('/I/want/title/', function(req, res) {
-	getTitles(req.query.address, function(titles) {
-		res.render('titles', {
+	var promises = req.query.address.map(function(link){
+	  return getTitle(link);
+	});
+	RSVP.all(promises).then(function(titles) {
+	  res.render('titles', {
 			titles: titles
-		});		
+		});
 	});
 });
 
 app.listen(3000);
 
-// methods get titles from remote sites
-function getTitles(links, callback) {
-	var url    = require('url');
-	var http   = require('http');
-	var https  = require('https');		
-  var titles = [];
-	links.forEach(function(link) {
+// methods to get titles from remote sites
+var url    = require('url');
+var http   = require('http');
+var https  = require('https');
+var getTitle = function(link) {
+	var promise = new RSVP.Promise(function(resolve, reject){	
 		if(link === undefined || link === '') {
-	    titles.push([link, 'Invalid domain']);
-	    triggerCallbackFunction(links, titles, callback);
+	    resolve([link, 'Invalid domain']);
 	  }
 		var url_parts = url.parse(link);
 	  var options = {
@@ -37,32 +39,24 @@ function getTitles(links, callback) {
 	  protocol.get(options, function(result) {
 	  	  statusCode = result.statusCode;
 	  	  if(statusCode<200 || statusCode>299) {
-	  	  	titles.push([link, 'Error::statusCode: ' + statusCode]);
-	  	  	triggerCallbackFunction(links, titles, callback);
+	  	  	resolve([link, 'Error::statusCode: ' + statusCode]);
 	  	  } else {
 	  	  	var body = '';
 		      result.on("data", function(chunk) {
 		    		body += chunk;
 		  		});
 		  		result.on('end', function() {
-				    titles.push([link, getTitleFromResponse(body)]);
-				    triggerCallbackFunction(links, titles, callback);
+				    resolve([link, getTitleFromResponse(body)]);
 				  });
 	  	  }
 	  }).on('error', function(e) {
-	    titles.push([link, e.message]);
-	    triggerCallbackFunction(links, titles, callback);
+	    resolve([link, e.message]);
 	  });
-	});
+	});  
+	return promise;
 }
 function getTitleFromResponse(response) {
   var res_arr = response.match(/<title[^>]*>([^<]+)<\/title>/);
   return (Array.isArray(res_arr) ? res_arr[1] : 'Not found');
-}
-
-function triggerCallbackFunction(links, titles, callback){
-	if(titles.length == links.length) { // Trigger callback function if all links are processed
-		callback(titles);
-	}
 }
 // end of methods get titles from remote site
